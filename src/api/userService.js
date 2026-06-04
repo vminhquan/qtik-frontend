74ab2149ecb4c67ee66c7c84efb79a5c048214d0
huyen64 <162551918+vminhquan@users.noreply.github.com>
2026-06-04 11:40:54 +0700

    x

--- src/api/userService.js
+++ src/api/userService.js
@@ -1,20 +1,22 @@
 import axiosClient from "./axiosClient";
 import { USER_API } from "../constants/apiEndpoints";
 
+const publicRequest = { skipAuth: true };
+
 export const userService = {
-  register: (data) => axiosClient.post(USER_API.REGISTER, data),
-  verifyOTP: (data) => axiosClient.post(USER_API.VERIFY, data),
+  register: (data) => axiosClient.post(USER_API.REGISTER, data, publicRequest),
+  verifyOTP: (data) => axiosClient.post(USER_API.VERIFY, data, publicRequest),
   getUsers: (params) => axiosClient.get(USER_API.GET_ALL, { params }),
   getUserById: (id) => axiosClient.get(USER_API.DETAIL(id)),
   updateUser: (id, data) => axiosClient.put(USER_API.DETAIL(id), data),
   deleteUser: (id) => axiosClient.delete(USER_API.DETAIL(id)),
   updateProfile: (data) => axiosClient.put(USER_API.ME, data),
-  login: (data) => axiosClient.post(USER_API.LOGIN, data),
-  refreshToken: (data) => axiosClient.post(USER_API.REFRESH_TOKEN, data),
+  login: (data) => axiosClient.post(USER_API.LOGIN, data, publicRequest),
+  refreshToken: (data) => axiosClient.post(USER_API.REFRESH_TOKEN, data, publicRequest),
   logout: () => axiosClient.post(USER_API.LOGOUT),
-  forgotPassword: (data) => axiosClient.post(USER_API.FORGOT_PASSWORD, data),
-  resetPassword: (data) => axiosClient.post(USER_API.RESET_PASSWORD, data),
-  resendOTP: (data) => axiosClient.post(USER_API.RESEND_OTP, data),
+  forgotPassword: (data) => axiosClient.post(USER_API.FORGOT_PASSWORD, data, publicRequest),
+  resetPassword: (data) => axiosClient.post(USER_API.RESET_PASSWORD, data, publicRequest),
+  resendOTP: (data) => axiosClient.post(USER_API.RESEND_OTP, data, publicRequest),
 };
 
 export const {
--- src/constants/apiEndpoints.js
+++ src/constants/apiEndpoints.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8000/api";
+export const BASE_URL = "https://fastapi-project-6qt6.onrender.com/api";
 
 export const USER_API = {
   REGISTER: "/users/register",
